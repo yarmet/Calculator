ac659419be8b4f9fb3944064703e194dec0c4288
yarmet <yarmet84@mail.ru>
2015-02-07 04:38:05 +0300

    deleted comment

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,9 +1,5 @@
 package com.company;
 
-/**
- * Программа калькулятора использующего обратную польскую запись.
- * https://ru.wikipedia.org/wiki/%D0%9E%D0%B1%D1%80%D0%B0%D1%82%D0%BD%D0%B0%D1%8F_%D0%BF%D0%BE%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F_%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C
- */
 
 import java.math.BigDecimal;
 import java.util.*;
